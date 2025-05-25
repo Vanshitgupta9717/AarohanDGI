d95b7084a6c9e26d462be4740ab3d960bc9dc6ca
Abhinav7506 <abhinavsinghal283@gmail.com>
2025-05-25 12:03:47 +0530

    updated event

--- src/Components/Events/LogoDesign.jsx
+++ src/Components/Events/LogoDesign.jsx
@@ -13,7 +13,7 @@ const LogoDesign = () => {
                 <div id="CNdetails">
                     🖌️ Aarohan Logo Making Competition
                     🎉 Get ready to unleash your creativity!
-                    We at Aarohan, the Coding Club of DGI, are organizing a "Logo Making Competition"  to discover the ingenious creativity hidden within our campus and beyond. This is your chance to leave your mark — literally! Design the logo that will represent the identity of our club!<br /><br />
+                    !<br /><br />
                     Before submitting the form: <br />
                     • Take a screenshot of your filled form (just before submitting).<br />
                     • Post it as a story on Instagram.<br />
@@ -21,9 +21,10 @@ const LogoDesign = () => {
                     (We’ll be resharing all valid story entries!)<br />
 
                     ❗ Note: If you don’t upload the story or don’t tag us, your form will not be accepted.<br /> <br />
-
+                    📅 Important Dates <br />
+                    * Form Opens: 25th May 2025 (Sunday) [ 12:00 PM ]<br />
+                    * Last Date to Submit: 27th May 2025 (Tuesday) [ 06:00 PM ]<br /><br />
                     📁 File Submission Requirements<br />
-                    * Upload your final logo design at the end of this form.<br />
                     * File Format: JPEG (.jpg / .jpeg) or PDF form only<br />
                     * Ensure the file is of high quality and clearly visible.<br /><br />
 
@@ -33,12 +34,18 @@ const LogoDesign = () => {
                     * Relevance to coding and innovation<br />
                     * Visual aesthetics and impact<br />
                     * Scalability & adaptability as a logo<br /><br />
-                    📅 Important Dates <br />
-                    * Form Opens: 25th May 2025 (Sunday) [ 12:00 PM ]<br />
-                    * Last Date to Submit: 27th May 2025 (Tuesday) [ 06:00 PM ]<br />
-                    💻  VENUE: Online <br /><br />
-                    {/* 📤Submission Form : <a href="https://docs.google.com/forms/d/e/1FAIpQLSciA1RVpbsKKt8MUymCnSM5JgLAYTxguFRD8wY7GPB0AFDb1Q/viewform?usp=header">Click Here</a> <br /> */}
+                    
+                    📤Submission Form : <a href="https://docs.google.com/forms/d/e/1FAIpQLSciA1RVpbsKKt8MUymCnSM5JgLAYTxguFRD8wY7GPB0AFDb1Q/viewform">Click Here</a> <br />
                     <br /><br />
+                    🏆 Rewards <br />
+                    The winner's logo will be officially featured across our platforms<br />
+                    . Shoutout & certificates!<br />
+                    . Winning rewards<br />
+                    1st 🥇-  Aarohan t shirt<br />
+                    2nd 🥈 - Cup<br />
+                    3rd  🥉- Diary<br /><br />
+
+                    🔊 - The Results will be announced on 29th May Evening.<br /><br />
                     ⚠️ EXCLUSIVE ELIGIBILITY CRITERIA ! <br /><br />
                     🔹 You will be able to participate ONLY IF you have followed our social media handles! <br /> <br />
                     📍 Stay connected & never miss upcoming career-changing events & insights! <br />
